85d95662d5cdd125ae86d8dc3ee4a1a13984dfca
brnsbrn <sabranlaks79@gmail.com>
2023-04-09 12:48:03 +0800

    api ecommerce rabot

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::group([
+    'middleware' => 'api',
+], function(){
+    Route::resources([
+        'categories' => CategoryController::class
+    ]);
+});
